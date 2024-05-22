cafee0fb6661c97a5d0716031781210ee3ff8c3f
ranggawan09 <ranggawan09@gmail.com>
2024-05-22 08:11:35 +0700

    update data in ci

--- project-root/app/Config/Routes.php
+++ project-root/app/Config/Routes.php
@@ -8,6 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Page::index');
 $routes->get('/books/index', 'Books::index');
 $routes->get('/books/create', 'Books::create');
+$routes->get('/books/edit/(:segment)', 'Books::edit/$1');
 $routes->get('/books/save', 'Books::save');
 $routes->delete('/books/(:num)', 'Books::delete/$1');
 $routes->get('/books/(:any)', 'Books::detail/$1');
